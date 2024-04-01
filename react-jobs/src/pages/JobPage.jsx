b548004b577da43f484919022f37385448b43376
Kavindu Fernando <kavindu2331@gmail.com>
2024-04-01 18:46:45 +0530

    feature: disabled the use of vars

--- react-jobs/src/pages/JobPage.jsx
+++ react-jobs/src/pages/JobPage.jsx
@@ -1,3 +1,4 @@
+/* eslint-disable no-unused-vars */
 /* eslint-disable react/prop-types */
 import { useParams, useLoaderData, useNavigate } from 'react-router-dom';
 import { FaArrowLeft, FaMapMarker } from 'react-icons/fa';
